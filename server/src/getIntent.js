9988a56e767e1f955ed2e52e594a7204e8b8d16b
Nanda Vikas <nanda.vikas@yellowmessenger.com>
2021-07-11 21:55:33 +0530

    added API for model to decrease bot response time

--- server/src/getIntent.js
+++ server/src/getIntent.js
@@ -1,24 +1,43 @@
 const spawn = require('child_process').spawn
+const request = require('request');
 
 var prediction = {}
 const pullIntent = (userMessage) => {
-    return new Promise((resolve,reject) => {
-        let pythonFile = __dirname + "/nlp/prediction.py"
-        let pythonModel = spawn('python', [pythonFile, userMessage])
-        pythonModel.stdout.on('data', (data) => {
-            // console.log(data.toString(), "Data from python")
-            data = JSON.parse(data)
-            resolve(data)
-        })
-
-        pythonModel.stderr.on('data', (data) => {
-            console.error(`stderr: ${data}`);
-            reject(data.toString())
+    return new Promise((resolve, reject) => {
+        const options = {
+            'method': 'GET',
+            'url': `http://localhost:8000/predict?utterance=${userMessage}`,
+            'headers': {
+            }
+        };
+        request(options, function (error, response) {
+            if (error) {
+                reject(error);
+            }
+            else {
+                const body= JSON.parse(response.body);
+                console.log("Body from predict API", body.intent);
+                console.log("Body from predict API", body.confidence);
+                resolve(body);
+            }
         });
 
-        pythonModel.on('close', (code) => {
-            console.log(`child process exited with code ${code}`);
-        });
+        // let pythonFile = __dirname + "/nlp/prediction.py"
+        // let pythonModel = spawn('python', [pythonFile, userMessage])
+        // pythonModel.stdout.on('data', (data) => {
+        //     // console.log(data.toString(), "Data from python")
+        //     data = JSON.parse(data)
+        //     resolve(data)
+        // })
+
+        // pythonModel.stderr.on('data', (data) => {
+        //     console.error(`stderr: ${data}`);
+        //     reject(data.toString())
+        // });
+
+        // pythonModel.on('close', (code) => {
+        //     console.log(`child process exited with code ${code}`);
+        // });
     })
 }
 
--- server/src/nlp/prediction.py
+++ server/src/nlp/prediction.py
@@ -10,44 +10,86 @@ from torch.utils.data import DataLoader, Dataset
 from cnn_model import NeuralNet
 from nlp_utils import tokenize, stem, bag_of_words
 from prediction_helper import prediction
-import json  
+from flask import Flask, request
+import json
+import os
+from pathlib import Path
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-FILE = "config.pth"
+app = Flask(__name__)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+root = os.getcwd()
+file_path = Path(root)
+# print(file_path.parent.parent)
+FILE = os.path.join(file_path.parent.parent, "config.pth")
 config = torch.load(FILE)
 
-input_size = config['input_size']
-hidden_size = config['hidden_size']
-output_size = config['output_size']
-all_words = config['all_words']
-slugs = config['slugs']
-model_state = config['model_state']
+input_size = config["input_size"]
+hidden_size = config["hidden_size"]
+output_size = config["output_size"]
+all_words = config["all_words"]
+slugs = config["slugs"]
+model_state = config["model_state"]
 
 model = NeuralNet(input_size, hidden_size, output_size)
 model.load_state_dict(model_state)
 model.eval()
 
 # Get prediction from model
-utterance = sys.argv[1]
-utterance = tokenize(utterance)
-X = bag_of_words(utterance, all_words)
-X = X.reshape(1, X.shape[0])
-X = torch.from_numpy(X)
+# utterance = sys.argv[1]
+# utterance = tokenize(utterance)
+# X = bag_of_words(utterance, all_words)
+# X = X.reshape(1, X.shape[0])
+# X = torch.from_numpy(X)
 
-output = model(X)
-_, predicted = torch.max(output, dim=1)
+# output = model(X)
+# _, predicted = torch.max(output, dim=1)
 
-probabilities = torch.softmax(output, dim=1)
-confidence = probabilities[0][predicted.item()]
+# probabilities = torch.softmax(output, dim=1)
+# confidence = probabilities[0][predicted.item()]
 
-intent = slugs[predicted.item()]
-final_prediction = {
-    "intent": intent,
-    "confidence": confidence.item(),
-    "entities": prediction(str(utterance))
-}
+# intent = slugs[predicted.item()]
+# final_prediction = {
+#     "intent": intent,
+#     "confidence": confidence.item(),
+#     "entities": prediction(str(utterance)),
+# }
 
-final_prediction = json.dumps(final_prediction, indent = 4)  
+# final_prediction = json.dumps(final_prediction, indent=4)
 
 
-print(final_prediction)
+# print(final_prediction)
+
+
+@app.route('/predict', methods=["GET"])
+def predict():
+    utterance = request.args.get('utterance')
+    print(utterance)
+    # return utterance
+    # if utterance is None:
+    #     return json.dumps({"error": "An utterance is required to get prediction"})
+    # else:
+    utterance = tokenize(utterance)
+    X = bag_of_words(utterance, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    probabilities = torch.softmax(output, dim=1)
+    confidence = probabilities[0][predicted.item()]
+
+    intent = slugs[predicted.item()]
+    final_prediction = {
+        "intent": intent,
+        "confidence": confidence.item(),
+        "entities": prediction(str(utterance)),
+    }
+
+    final_prediction = json.dumps(final_prediction, indent=4)
+    return final_prediction
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
